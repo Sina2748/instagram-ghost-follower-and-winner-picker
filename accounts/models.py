b2bd82ef9ff9d9bc86a0a72a3afdc02625769812
Sina Samii <sina.sam@gmail.com>
2021-11-09 00:04:20 +0330

    1st

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+# accounts/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    pass
