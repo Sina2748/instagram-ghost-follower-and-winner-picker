8bfb8ae88cf70ab0387a3554c9edda5ace8307d9
Sina Samii <sina.sam@gmail.com>
2021-11-09 00:28:06 +0330

    2nd

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
 # Create your tests here.
+# pages/tests.py
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+
+class HomepageTests(SimpleTestCase):
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self): # new
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# pages/views.py
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
