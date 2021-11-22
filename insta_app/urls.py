5f58558edbfd8cbdd862d4a1ceafd77ce2864b5c
Sina Samii <sina.sam@gmail.com>
2021-11-22 20:20:45 +0330

    orderd list and win url

--- insta_app/urls.py
+++ insta_app/urls.py
@@ -1,8 +1,9 @@
 # books/urls.py
 from django.urls import path
-from .views import add_view
+from .views import add_view, win_view
 
 urlpatterns = [
     path('add/', add_view, name='add'),
+    path('add/winners/', win_view, name='win'),
 
 ]
--- insta_app/views.py
+++ insta_app/views.py
@@ -12,16 +12,9 @@ import instaloader
 import random
 
 # Create your views here.
-
-
-
-def add_view(request):
-    context ={}
-    print("hi1")
-    # create object of form
+def win_view(request):
+    context = {}
     form = AddCreateForm(request.POST or None, request.FILES or None)
-      
-    # check if form data is valid
     if form.is_valid():
         # save the form data to model
         print('hi3')
@@ -62,7 +55,7 @@ def add_view(request):
             print("Fetching likes of all posts of profile {}.".format(SHORTCODE))
             for post in profile.get_posts():
                 likes = post.from_shortcode(L.context, SHORTCODE).get_likes()
-                print(likes)
+                
 
             ran_like = []
             for like in likes:
@@ -74,52 +67,26 @@ def add_view(request):
 
         elif picker_kind == 'mentions':
             pass
-
-
-
-        #### instaloader ####   
-        # L = instaloader.Instaloader()
-        # # user_insta_ID = insta_model.objects.last()
-        # # user_insta_ID_str = str(user_insta_ID)
         
-        # PROFILE = user_insta_ID_str
-
-        # # Load session previously saved with `instaloader -l USERNAME`:
-        # L.login("anis2423f", "anisanisanis") 
-        # # USER = "anis2423f"
-        # # L.load_session_from_file(USER, " ~\books\insta_app\session-anis2423f ")
-
-        # profile = instaloader.Profile.from_username(L.context, PROFILE)
-
-        # likes = []
-        # print("Fetching likes of all posts of profile {}.".format(profile.username))
-        # for post in profile.get_posts():
-        #     print(post)
-        #     likes = likes + list(post.get_likes())
-
-        # print("Storing like into file.")
-        # with open("inactive-users.txt", 'w') as f:
-        #     for like in likes:
-        #         print(like.username, file=f)
-
-
-        # likes = set()
-        # print("Fetching likes of all posts of profile {}.".format(profile.username))
-        # for post in profile.get_posts():
-        #     print(post)
-        #     likes = likes | set(post.get_likes())     
-        
-        ####################
-
-        book_list = insta_model.objects.all()
-        # print(book_list)
-        context= {'book_list':book_list}
+        context= {'winner':winner}
         # print(context)
 
-        context['variable']= book_list
-        return render(request, "about.html", {'book_list':book_list})
+        return render(request, "about.html", context)
+    
+    # context ={}
+    # context['form']= ["No winners!"]
+    return render(request, "about.html")
+
+
+def add_view(request):
+    context ={}
+    print("hi1")
+    # create object of form
+    form = AddCreateForm(request.POST or None, request.FILES or None)
+      
+
 
     print('hi2')
     context['form']= form
-    # print(context)
+    # print(context['form'])
     return render(request, "insta_app_templates/add.html", context)
